9e1d88c212c6e7e9eb20067e6471ec7e6e92f9c6
AbdulArfath <arafathabdul80@gmail.com>
2026-05-15 21:15:28 +0530

    Day 19: Organizer Promo Code CRUD backend

--- app.py
+++ app.py
@@ -33,6 +33,7 @@ from app.routes.registration import registration_bp
 from app.routes.payment import payment_bp
 from app.routes.sessions import sessions_bp 
 from app.routes.speakers import speakers_bp
+from app.routes.promos import promos_bp
  
 app.register_blueprint(test_bp)
 app.register_blueprint(auth_bp)
@@ -48,6 +49,7 @@ app.register_blueprint(registration_bp)
 app.register_blueprint(payment_bp)
 app.register_blueprint(sessions_bp)
 app.register_blueprint(speakers_bp)
+app.register_blueprint(promos_bp)
 
 csrf.exempt(checkin_bp)
  # scanner JS sends token in header, not form
